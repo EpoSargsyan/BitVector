7a1fb90d42e5f0a72b7dd9c52a1802b66052b82f
EpremSargsyan <110049829+EpoSargsyan@users.noreply.github.com>
2023-01-18 14:06:57 +0400

    Update BItVector.cpp

--- BItVector.cpp
+++ BItVector.cpp
@@ -1,94 +1,56 @@
-#include "BitVector.h"
+#include "bitvector.h"
+#include <iostream>
 
-BitVector::BitVector() {
-    this->m_arr = nullptr;
-    this->m_size = 0;
+BitVector::BitVector() 
+: m_size{1}
+{
+    this->m_arr = new int[m_size];
 }
 
-BitVector::BitVector(const int& size) {
-    this->m_arr = new int[size];
-}
-
-BitVector::BitVector(const BitVector& rhs) {
-    this->clear();
-    this->m_arr = new int[rhs.m_size];
-    this->m_size = rhs.m_size;
-
-    for (int i = 0; i < this->m_size; ++i) {
-        this->m_arr[i] = rhs[i]; 
-    }
-}
-
-BitVector::BitVector(BitVector&& rhs) {
-    this->clear();
-    this->m_arr = rhs.m_arr;
-    this->m_size = rhs.m_size;
-    rhs.m_arr = nullptr;
-    rhs.m_size = 0;
+BitVector::BitVector(const int size)
+: m_size{size}
+{
+    this->m_arr = new int[m_size];
 }
 
 BitVector::~BitVector() {
-    clear();
-}
-
-void BitVector::clear() {
-    delete this->m_arr;
-    this->m_size = 0;
-    this->m_arr = nullptr;
-}
-
-BitVector& BitVector::operator=(const BitVector& rhs) {
     this->clear();
-    this->m_arr = new int[rhs.m_size];
-    this->m_size = rhs.m_size;
+}
 
-    for (int i = 0; i < this->m_size; ++i) {
-        this->m_arr[i] = rhs[i]; 
+void BitVector::set(const int position, const bool value) {
+    if (position >= m_size * sizeof(int) * 8) {
+        this->resize(position);
     }
-    return *this;
+    int ind = position / sizeof(int) * 8;
+    int pos = position - (ind * (sizeof(int) * 8));
+    m_arr[ind] |= (1 << pos);
 }
 
-BitVector& BitVector::operator=(BitVector&& rhs) {
-    if (this == &rhs) { return *this; }
-    this->clear();
-    this->m_arr = rhs.m_arr;
-    this->m_size = rhs.m_size;
-    rhs.m_arr = nullptr;
-    rhs.m_size = 0;
-    return *this;
-}
-
-int& BitVector::operator[](const int& index) {
-    return this->m_arr[index];
-}
-
-const int& BitVector::operator[](const int& index) const {
-    return this->m_arr[index];
-}
-
-void BitVector::resize(const int& new_size) {
-    int* changed_arr = new int[new_size];
-    for (int i = 0; i < this->m_size; ++i) {
-        changed_arr[i] = this->m_arr[i];
-    }
-
-    clear();
-
-    this->m_arr = new int[new_size];
-    this->m_arr = changed_arr;
-    this->m_size = new_size;
-    changed_arr = nullptr;
+bool BitVector::get(const int position) {
+    int ind = position / sizeof(int) * 8;
+    int pos = position - (ind * (sizeof(int) * 8));
+    return m_arr[ind] &= (1 << pos); 
 }
 
 const int BitVector::size() const {
     return this->m_size;
 }
 
-void BitVector::set(const size_t& index, const bool value) {
-    if (index > this->m_size) { resize(index + 1); }
-    this->m_arr[index] = value;
+void BitVector::resize(const int new_size) {
+
+    int tmp_size = (new_size / (sizeof(int) * 8));
+    int* tmp = new int[++tmp_size];
+    for (int i = 0; i < m_size; ++i) {
+        tmp[i] = m_arr[i];
+    }
+    this->clear();
+    m_arr = tmp;
+    tmp = nullptr;
+    this->m_size = tmp_size;
 }
 
-int BitVector::get(const int& index) {
-    return this->m_arr[index];
+void BitVector::clear() {
+    delete this->m_arr;
+    this->m_arr = nullptr;
+    this->m_size = 0;
 }
