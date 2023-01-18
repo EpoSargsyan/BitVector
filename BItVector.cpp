40e0595342ea6a2b08bdf46d7bd1d407d09546a8
EpremSargsyan <110049829+EpoSargsyan@users.noreply.github.com>
2023-01-18 17:17:56 +0400

    Update BItVector.cpp

--- BItVector.cpp
+++ BItVector.cpp
@@ -22,10 +22,13 @@ void BitVector::set(const int position, const bool value) {
         this->resize(position);
     }
     int ind = position / sizeof(int) * 8;
-    int pos = position - (ind * (sizeof(int) * 8));
-    if (value)  { m_arr[ind] |= (value << pos); }
-    else { m_arr[ind] &= (value << pos); }
-
+    int pos = ((ind + 1) * sizeof(int) * 8) - position;
+    if (value) {
+        m_arr[ind] |= (value << pos);
+    }
+    else {
+        m_arr[ind] &= ~(1 << pos);
+    }
 }
 
 bool BitVector::get(const int position) {
