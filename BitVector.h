980e2f7bdf2909452b6a74f329a76ac23cf7c844
EpremSargsyan <110049829+EpoSargsyan@users.noreply.github.com>
2023-01-18 14:07:35 +0400

    Update BitVector.h

--- BitVector.h
+++ BitVector.h
@@ -1,42 +1,26 @@
 #ifndef BITVECTOR_H
 #define BITVECTOR_H
 
-#include <iostream>
+#include <execution>
 
 class BitVector {
 public:
     BitVector();
-    BitVector(const int&);
-    BitVector(const BitVector&);
-    BitVector(BitVector&&);
+    BitVector(const int);
     ~BitVector();
 
 public:
-    void set(const size_t&, const bool);
-    int get(const int&);
+    void set(const int, const bool val = true);
+    bool get(const int);
     const int size() const;
     void clear();
-
-public:
-    int& operator[](const int&);
-    const int& operator[](const int&) const;
-    BitVector& operator=(const BitVector&);
-    BitVector& operator=(BitVector&&);
-
-public:
-    friend std::ostream& operator<<(std::ostream& stream, const BitVector& rhs) {
-        for (int i = rhs.size() - 1; i >= 0 ; --i) {
-            stream << rhs.m_arr[i];
-        } 
-        return stream;
-    }
-
+    
 private:
-    void resize(const int&);
+    void resize(const int);
 
 private:
     int* m_arr;
-    size_t m_size;
+    int m_size;
 };
 
 #endif // BITVECTOR_H
